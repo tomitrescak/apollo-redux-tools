f0aa51eec05ca6d66907f43c41b62c221a088d7d
Tomas Trescak <tomi.trescak@gmail.com>
2016-11-03 14:51:53 +1100

    1.2.0

--- src/mutation.ts
+++ src/mutation.ts
@@ -10,17 +10,28 @@ export interface IMutation {
   errorCallback?: (errors: any, dispatch: Function, state: () => any) => void;
   catchCallback?: (error: any, dispatch: Function, state: () => any) => void;
   finalCallback?: (dispatch: Function, state: () => any) => void;
+  optimisticResponse?: any;
 }
 
-export default function({ query, variables, optimisticCallback, thenCallback, errorCallback, catchCallback, finalCallback }: IMutation) {
+const regexp = /mutation ([\w_]*)/;
+
+export default function({ query, variables, optimisticCallback, thenCallback, errorCallback, catchCallback, finalCallback, optimisticResponse }: IMutation) {
   return (dispatch: Function, state: () => any): any => {
     if (optimisticCallback) {
       optimisticCallback(dispatch, state);
     }
 
+    let optimistic: any = null;
+    if (optimisticResponse) {
+      const mutationName = regexp.exec(query)[1];
+      optimistic = { __typename: 'Mutation' };
+      optimistic[mutationName] = optimisticResponse;
+    }
+
     config.apolloClient.mutate({
       mutation: gql`${query}`,
-      variables: variables
+      variables: variables,
+      optimisticResponse: optimistic
     }).then((graphQLResult: any) => {
       const { errors, data } = graphQLResult;
 
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -2,6 +2,5 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/isomorphic-fetch/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/redux/index.d.ts" />
 /// <reference path="modules/react-redux/index.d.ts" />
